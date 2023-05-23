981e1db90d21409dbec0ea992029aed4482da1bc
CampoSuarezJavierDavidCampus <campo.javier1502@gmail.com>
2023-05-23 16:23:20 -0500

    feat: :sparkles: agregar algunos estilos y validar formularios

--- dist/js/components/new-product.js
+++ dist/js/components/new-product.js
@@ -1,19 +1,54 @@
-export default class newProduct extends HTMLElement {
+export class NewProduct extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
   }
+
+  handleEvent(e){    
+      e.preventDefault();
+      if(e.type === 'submit') this.actions(e);
+  }
+
+  async actions(e){
+    let type = e.submitter.dataset.type;
+    type == 'delete'
+      ? this.delete(this)
+      : this.add();     
+  }
+
+  add(){    
+    console.log(this.validarsDatos())  
+  }
+
+  validarsDatos(){
+    console.log(new FormData())
+    const 
+      FORM = this.shadowRoot.querySelector('FORM'),
+      DATAFORM = new FormData(FORM),
+      DATA = DATAFORM;
+    console.log(DATA)
+    
+    return DATA
+  }
+
+  delete(){
+    this.remove();
+  }
+
   async getStyles() {
     return await (await fetch("dist/css/new-product.css")).text();
   }
   async getTemplate() {
-    let hrml = /* html */ `
+    let html = /* html */ `
         <style>
-            ${this.getStyles()}
+            ${await this.getStyles()}
         </style>
     `;
     html += await (await fetch("dist/templates/new-product.html")).text();
     return html;
   }
-  connectedCallback() {}
+  async connectedCallback() {
+    this.shadowRoot.innerHTML = await this.getTemplate();    
+    this.shadowRoot.querySelector('FORM').addEventListener('submit',this)
+  }
 }
